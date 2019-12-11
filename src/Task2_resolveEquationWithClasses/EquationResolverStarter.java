c2aad41e91520464591b32a3bfac80faaaeb0099
Tadashimi <tadashimi777@gmail.com>
2019-12-11 13:10:32 +0300

    Task 2. Quadratic equation with OOP.

---
+++ src/Task2_resolveEquationWithClasses/EquationResolverStarter.java
@@ -0,0 +1,12 @@
+package Task2_resolveEquationWithClasses;
+
+public class EquationResolverStarter {
+    public static void main(String[] args) {
+        QuadraticEquation quadraticEquation =
+                new QuadraticEquation(
+                        new InputData(1f, 2f, 3f)
+                );
+        OutputData result = quadraticEquation.resolve();
+        result.printSolutions();
+    }
+}
